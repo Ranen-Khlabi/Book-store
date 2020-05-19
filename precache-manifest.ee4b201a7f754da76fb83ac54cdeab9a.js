9f4d86e76d58c8e96a7256b933dfb83ac403a97f
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-05-19 03:43:20 +0300

    Updates

---
+++ precache-manifest.ee4b201a7f754da76fb83ac54cdeab9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36c65f873c5ffa16ec235ff2b736405",
+    "url": "/Book-store/index.html"
+  },
+  {
+    "revision": "54ee05ed243274444a0d",
+    "url": "/Book-store/static/css/2.3ea52fea.chunk.css"
+  },
+  {
+    "revision": "4b41505f6a1e70d449a9",
+    "url": "/Book-store/static/css/main.75bbb1a1.chunk.css"
+  },
+  {
+    "revision": "54ee05ed243274444a0d",
+    "url": "/Book-store/static/js/2.0209f667.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Book-store/static/js/2.0209f667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b41505f6a1e70d449a9",
+    "url": "/Book-store/static/js/main.428b9927.chunk.js"
+  },
+  {
+    "revision": "941be1fece26eea783ea",
+    "url": "/Book-store/static/js/runtime-main.17192253.js"
+  }
+]);
